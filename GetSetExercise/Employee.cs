2b0dc9eb1139f38d5e33c962ef09276374cbc05e
UWB <UWB@UWB-EMILY>
2016-11-15 16:04:46 -0500

    Add project files.

---
+++ GetSetExercise/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetExercise
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Email { get; set; }
+    }
+}
